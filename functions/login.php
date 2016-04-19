262239bcd01874365c22fde2ef3feecce3117da8
Felipe de Farias <felipedefarias@live.com>
2016-04-18 22:27:40 -0300

    Estiliza a tela de login

--- functions/login.php
+++ functions/login.php
@@ -6,6 +6,25 @@
  */
 
 function custom_login_logo() {
-	echo '<style> .login h1 a { background-image: url("' . get_bloginfo('template_directory') . '/dist/images/login.png") } </style>';
+	echo 
+	'<style> 
+		body, html { background-color: #151c24; }
+
+		.login #login_error {
+			border-top: 5px solid #dc3232;
+			border-left: 0;
+			padding: 0;
+		}
+
+		.login form { margin-top: 0; }
+
+		.login h1 a {
+			background-image: url("' . get_bloginfo('template_directory') . '/dist/images/login__logo.svg");
+			background-repeat: no-repeat;
+			width: 100%;
+			background-size: 70%;
+			background-position: center;
+		}
+	</style>';
 }
 add_action('login_head', 'custom_login_logo');
