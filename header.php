aafcc3bfcc64bdb055cfda965e0c85fdd3e43768
Felipe de Farias <felipedefarias@live.com>
2016-04-23 23:45:13 -0300

    favicons

--- header.php
+++ header.php
@@ -5,7 +5,31 @@
 	<link rel="alternate" href="http://vicium.com.br" hreflang="pt-br" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0" />
 
-	<title><?php if (function_exists('is_tag') && is_tag()) { echo 'Arquivos '.$tag.' - '; } elseif (is_archive()) { wp_title(''); echo ' Arquivos - '; } elseif (is_search()) { echo 'Resultado de busca '.esc_html($s).' - '; } elseif (!(is_404()) && (is_single()) || (is_page())) { wp_title(''); echo ' '; } elseif (is_404()) { echo 'P&aacute;gina n&aacute;o encontrada - '; } if (is_home()) { bloginfo('name'); echo ' - '; bloginfo('description'); } else { bloginfo('name'); echo ' - '; bloginfo('description'); } ?></title>
+	<title><?php title(); ?></title>
+
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/apple-touch-icon-180x180.png">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/favicon-194x194.png" sizes="194x194">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/android-chrome-192x192.png" sizes="192x192">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/favicon-16x16.png" sizes="16x16">
+	<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/manifest.json">
+	<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/safari-pinned-tab.svg" color="#161c23">
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/favicon.ico">
+	<meta name="apple-mobile-web-app-title" content="Vicium">
+	<meta name="application-name" content="Vicium">
+	<meta name="msapplication-TileColor" content="#161c23">
+	<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/mstile-144x144.png">
+	<meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/dist/images/favicon/browserconfig.xml">
+	<meta name="theme-color" content="#161c23">
 
 	<?php wp_head(); ?>
 </head>
