f99553a06f5073f97619bd56f2a00850b64aae8f
Felipe de Farias <felipedefarias@live.com>
2016-04-23 23:45:37 -0300

    Começa o dev da página de reviews

--- single-post_analises.php
+++ single-post_analises.php
@@ -6,10 +6,10 @@
 
 			<article class="single-analises__article">
 
+				<!-- hader do artigo -->
 				<header class="single-analises__header">
-
 					<figure class="single-analises__thumbnail">
-						<?php echo wp_get_attachment_image( get_post_thumbnail_id(), 'single-analises__thumbnail'); ?>
+						<?php echo wp_get_attachment_image( get_post_thumbnail_id(), 'single-full'); ?>
 					</figure>
 
 					<div class="single-analises__header--container container">
@@ -18,15 +18,13 @@
 							<h1 class="single-analises__title">Análise: <?php the_title(); ?></h1>
 
 							<div class="single-analises__data">
-								<time class="single-analises__data--time" datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('d M Y'); ?></time>
+								<time class="single-analises__data--time" datetime="<?php the_time('Y-m-d'); ?>" pubdate><?php the_time('d M Y'); ?></time>
 								<div  class="single-analises__data--author">Por <strong><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a></strong></div>
 								<div  class="single-analises__data--views"><span class="icon__views"><svg width="16" height="11"><use xlink:href="<?php echo get_template_directory_uri(); ?>/dist/images/sprite.svg#icon__views" /></svg></span>13.566</div>
 							</div>
 
 							<div class="single-analises__datasheet">
-
 								<?php 
-
 									$datasheet = array( 
 										'tax_generos' => 'tax', 
 										'tax_plataformas' => 'tax', 
@@ -37,7 +35,6 @@
 									);
 
 									foreach ($datasheet as $data => $type) {
-
 										if ($type == 'tax') {
 											
 											if ( !empty( get_the_term_list( $post->ID, $data ) ) ) {
@@ -59,24 +56,28 @@
 													echo '<dd class="single-analises__datasheet--content">' . $field['value'] . '</dd>';
 												echo '</dl>';
 											endif;
-
 										}
-			
 									}
-
 								?>
-
 							</div>
-
 						</div>
 					</div>
-
 				</header>
 
-				<div class="container">
+				<!-- Conteudo do artigo -->
+				<div class="single-analises__text container">
 
-				<?php the_content(); ?>
+					<?php require_once( get_template_directory() . '/_share-buttons.php' ); ?>
+
+					<div class="single-analises__content single__content">
+						<?php the_content(); ?>
+					</div>
+				
 				</div>
+
+				<footer class="single-analises__footer">
+					
+				</footer>
 
 			</article>
 
