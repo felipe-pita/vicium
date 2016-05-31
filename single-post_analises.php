d6f634b085ccebb6f363074f79596a0ff467289f
Felipe de Farias <felipedefarias@live.com>
2016-05-31 11:57:47 -0300

    W3C

--- single-post_analises.php
+++ single-post_analises.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?> 
 
-	<main class="page-main single-analises" role="main">
+	<main class="page-main single-analises">
 
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
